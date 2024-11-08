fee6e1c1bbf65376e62b8632db12451be569faa2
GilmerFranco <gilmerfranko@hotmail.com>
2024-11-08 16:45:39 -0400

    add: Archivo de configuracion de la base de datos

---
+++ config.db.php
@@ -0,0 +1,19 @@
+<?php defined('VCO') || exit;
+
+/**
+ *=======================================================
+ *  VCO Project
+ *-------------------------------------------------------
+ * @author Gilmer Franco <gil2017.com@gmail.com>
+ *=======================================================
+ *
+ * @Description Parámetros de conexión para la base de datos
+ *
+ *
+ */
+
+//'p:localhost';  // PERSISTENTE
+$db['hostname'] = 'localhost';    // Nombre del servidor MySQL
+$db['database'] = 'ryctv_cursosonline';    // Nombre de la base de datos
+$db['username'] = 'root';      // Nombre del usuario de la base de datos
+$db['userpass'] = '';				// Contraseña del usuario de la base de datos
