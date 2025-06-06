63a4ceb5ccaa9f3fb33381afab8c541961305abd
GilmerFranco <gilmerfranko@hotmail.com>
2025-06-06 14:40:34 -0400

    fix: Añade verificación de permisos para mostrar el botón de edición en el curso

--- modules/courses/view/course.c.html.php
+++ modules/courses/view/course.c.html.php
@@ -16,11 +16,13 @@
 <div id="course<?= $course['id'] ?>" class="course-item" style="display: flex; flex-direction: column; align-items: center;max-width: max-content;" onclick="location.href='<?= gLink('courses/view.course', ['course_id' => $course['id']]) ?>'">
 
   <div class="course-image">
-    <div class="course-card-action" style="position: absolute;margin-top: 0px;margin-left: 227px;z-index: 1;text-align: right;">
-      <a class="btn btn-small waves-effect waves-light blue" href="<?= gLink('admin/edit.course', ['course_id' => $course['id']]) ?>">
-        <i class="material-icons">edit</i>
-      </a>
-    </div>
+    <?php if ($session->is_admod): ?>
+      <div class="course-card-action" style="position: absolute;margin-top: 0px;margin-left: 227px;z-index: 1;text-align: right;">
+        <a class="btn btn-small waves-effect waves-light blue" href="<?= gLink('admin/edit.course', ['course_id' => $course['id']]) ?>">
+          <i class="material-icons">edit</i>
+        </a>
+      </div>
+    <?php endif; ?>
     <img src="<?= $config['courses_url'] . '/' . $course['image'] ?>">
   </div>
   <div class="card-content center" style="margin-top:8px;">
